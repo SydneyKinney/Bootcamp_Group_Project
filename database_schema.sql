9e32f4d1f2979c61ae398d27b1c201a0f2b77ff0
DanielBergan <daniel.bergan86@gmail.com>
2022-10-23 17:44:04 -0700

    added database schema

---
+++ database_schema.sql
@@ -0,0 +1,18 @@
+Google_playstore
+-
+app_name varchar
+maximum_installs int
+category_id varchar
+rating int
+price int
+rating_count int
+
+Price_install
+-
+price int
+maximum_install int
+
+Category
+-
+category varchar
+category_id varchar
